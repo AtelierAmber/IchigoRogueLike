c3f09cb6ca1e9fef8fe554a4d5d1d8195688ee68
Amber <aqscode@gmail.com>
2023-11-19 16:34:33 -0700

    Adding Resource Libraries

---
+++ Ichigo.Engine/MapObjects/Components/EntityComponentLibrary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ichigo.Engine.MapObjects.Components
+{
+  public static class EntityComponentLibrary
+  {
+    internal readonly static Dictionary<string, Type> Components = new();
+
+    public static Type GetComponentType(string name) => Components[name];
+
+    public static void RegisterType<T>(string name) => RegisterType(typeof(T), name);
+    public static void RegisterType(Type t, string name) => Components.Add(name, t);
+  }
+}
---
+++ Ichigo.Engine/MapObjects/TerrainLibrary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SadRogue.Integration;
+
+namespace Ichigo.Engine.MapObjects
+{
+  public static class TerrainLibrary
+  {
+    internal readonly static Dictionary<string, Func<RogueLikeCell>> Components = new();
+
+    public static Func<RogueLikeCell> GetComponentType(string name) => Components[name];
+
+    public static void RegisterTerrain(Func<RogueLikeCell> terrainBuilder, string name) => Components.Add(name, terrainBuilder);
+  }
+}
